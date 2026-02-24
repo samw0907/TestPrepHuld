28347c4e6a6d43e6fb10ab69004a121e3a2fe283
Sam Williamson <swilliamson_0907@outlook.com>
2026-02-24 14:51:33 +0200

    starting python prep questions

---
+++ python/test1.py
@@ -0,0 +1,24 @@
+#test1.py
+
+# Create three variables:
+#   - age         (a whole number)
+#   - temperature (a decimal number)
+#   - name        (a piece of text)
+#
+# Then print all three using f-strings.
+#
+# Example output:
+#   Age: 28
+#   Temperature: 36.6
+#   Name: Sam
+
+
+# Your code here
+
+age = 34
+temperature = -15.3
+name = "Sam"
+
+print(f"Age: {age}")
+print(f"Temperature: {temperature}")
+print(f"Name: {name}")
