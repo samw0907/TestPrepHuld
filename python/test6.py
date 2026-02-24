1abbc1f1d8762586429fc392e18c6289af914872
Sam Williamson <swilliamson_0907@outlook.com>
2026-02-24 15:26:47 +0200

    test6

---
+++ python/test6.py
@@ -0,0 +1,25 @@
+# test6.py
+
+# Create a dictionary called person with three keys:
+#   "name"  -> your choice of name (string)
+#   "age"   -> your choice of age (int)
+#   "city"  -> your choice of city (string)
+#
+# Print each value using its key
+#
+# Expected output (example):
+#   Sam
+#   28
+#   Helsinki
+
+# Your code here
+
+person = {
+    "name": "Sam",
+    "age": 34,
+    "city": "Espoo"
+}
+
+print(person["name"])
+print(person["age"])
+print(person["city"])
