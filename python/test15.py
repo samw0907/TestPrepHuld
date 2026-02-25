804e223432622effdd124d8afb0f5d7e4a2fb8af
Sam Williamson <swilliamson_0907@outlook.com>
2026-02-25 14:53:22 +0200

    few more qs!

---
+++ python/test15.py
@@ -0,0 +1,26 @@
+# test15.py
+
+# Create a 2D list called grid with 3 rows and 3 columns:
+#   [1, 2, 3]
+#   [4, 5, 6]
+#   [7, 8, 9]
+#
+# Print the value at row 0, column 2
+# Print the value at row 2, column 0
+# Print the entire middle row
+#
+# Expected output:
+#   3
+#   7
+#   [4, 5, 6]
+
+# Your code here
+grid = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(grid[0][2])
+print(grid[2][0])
+print(grid[1])
---
+++ python/test16.py
@@ -0,0 +1,22 @@
+# test16.py
+
+# Create the same 3x3 grid as last time:
+#   [1, 2, 3]
+#   [4, 5, 6]
+#   [7, 8, 9]
+#
+# Loop through every element and print each value
+#
+# Expected output:
+#   1
+#   2
+#   3
+#   4
+#   5
+#   6
+#   7
+#   8
+#   9
+
+# Your code here
+
